8261811ee2f0c2656291dac908c6587991b7e301
nennenwodo <nennenwodo@gmail.com>
2018-12-28 22:01:53 +0100

    Added category list

--- app/src/main/java/com/adoranwodo/play/Utility/Extensions.kt
+++ app/src/main/java/com/adoranwodo/play/Utility/Extensions.kt
@@ -24,16 +24,30 @@ fun Category.resource(): Category {
     val category = this
 
     when (category.name) {
-        "House Arryn" -> return Category(category.name, R.drawable.arryn, R.color.bgArryn, R.color.txtArryn)
-        "House Baratheon" -> return Category(category.name, R.drawable.baratheon, R.color.bgBaratheon, R.color.txtBaratheon)
-        "House Tarly" -> return Category(category.name, R.drawable.tarly, R.color.bgTarly, R.color.txtTarly)
-        "House Lannister" -> return Category(category.name, R.drawable.lannister, R.color.bgLannister, R.color.txtLannister)
-        "House Bolton" -> return Category(category.name, R.drawable.bolton, R.color.bgBolton, R.color.txtBolton)
-        "House Martell" -> return Category(category.name, R.drawable.martell, R.color.bgMartell, R.color.txtMartell)
-        "House Targaryen" -> return Category(category.name, R.drawable.targaryen, R.color.bgTargaryen, R.color.txtTargaryen)
-        "House Stark" -> return Category(category.name, R.drawable.stark, R.color.bgStark, R.color.txtStark)
-        "House Tully" -> return Category(category.name, R.drawable.tully, R.color.bgTully, R.color.txtTully)
-        "House Greyjoy" -> return Category(category.name, R.drawable.greyjoy, R.color.bgGreyjoy, R.color.txtGreyjoy)
-        else -> return Category(category.name,0, 0, 0)
+        "House Arryn" -> return category.update(R.drawable.arryn, R.color.bgArryn, R.color.txtArryn )
+        "House Baratheon" -> return category.update(R.drawable.baratheon, R.color.bgBaratheon, R.color.txtBaratheon)
+        "House Tarly" -> return category.update(R.drawable.tarly, R.color.bgTarly, R.color.txtTarly)
+        "House Lannister" -> return category.update(R.drawable.lannister, R.color.bgLannister, R.color.txtLannister)
+        "House Bolton" -> return category.update(R.drawable.bolton, R.color.bgBolton, R.color.txtBolton)
+        "House Martell" -> return category.update(R.drawable.martell, R.color.bgMartell, R.color.txtMartell)
+        "House Targaryen" -> return category.update(R.drawable.targaryen, R.color.bgTargaryen, R.color.txtTargaryen)
+        "House Stark" -> return category.update(R.drawable.stark, R.color.bgStark, R.color.txtStark)
+        "House Tully" -> return category.update(R.drawable.tully, R.color.bgTully, R.color.txtTully)
+        "House Greyjoy" -> return category.update(R.drawable.greyjoy, R.color.bgGreyjoy, R.color.txtGreyjoy)
+        else -> return category
     }
+}
+
+/**
+ * Extension helper method for updating a category instance
+ */
+private fun Category.update(image: Int, backgroundColor: Int, textColor: Int): Category{
+    val category = this
+
+    category.backgroundColor = backgroundColor
+    category.image = image
+    category.textColor = textColor
+
+    return category
+    //return Category(this.name, backgroundColor, image, textColor)
 }
--- app/src/main/java/com/adoranwodo/play/adapter/CategoryRecyclerAdapter.kt
+++ app/src/main/java/com/adoranwodo/play/adapter/CategoryRecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package com.adoranwodo.play.adapter
 
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
@@ -31,7 +33,7 @@ class CategoryRecyclerAdapter (private val categories: ArrayList<Category>)
     class CategoryHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
 
         private var view: View = v
-        private var category: Category? = null
+        private var category: Category = Category("")
 
 
         init {
@@ -43,16 +45,26 @@ class CategoryRecyclerAdapter (private val categories: ArrayList<Category>)
             Log.d("RecyclerView", "CLICK!")
             /*
             val context = itemView.context
-            val showPhotoIntent = Intent(context, PhotoActivity::class.java)
-            showPhotoIntent.putExtra(PHOTO_KEY, photo)
-            context.startActivity(showPhotoIntent)
+            val showGameActivity = Intent(context, GameActivity::class.java)
+            context.startActivity(showGameActivity)
             */
 
         }
 
         fun bind(category: Category) {
-            this.category = category
-            view.itemImage.setImageResource(category.resource().image)
+            this.category = category.resource()
+            view.itemImage.setImageResource(this.category.image)
+            view.itemBackground.setBackgroundResource(this.category.backgroundColor)
+            view.itemText.setTextColor(ContextCompat.getColor(itemView.context, this.category.textColor))
+            view.itemText.text = this.category.name
+
+
+            val gd = GradientDrawable(GradientDrawable.Orientation.TL_BR,
+                     intArrayOf(ContextCompat.getColor(itemView.context,android.R.color.darker_gray),
+                             ContextCompat.getColor(itemView.context,this.category.backgroundColor)))
+            gd.cornerRadius = 0f
+            view.itemBackground.background = gd
+
 
         }
 
